9b5d8b605a233d5a11d7839b7b86b0a594c6f0a9
Tunahan Yazar <93399467+tunahanyazar0@users.noreply.github.com>
2024-10-30 14:39:28 +0300

    Update HomePage.js

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,43 +1,54 @@
 // HomePage.js
-import React from "react";
-//import PopularItem from './PopularItem'; // Import the PopularItem component
-import './HomePage.css'; // Import the CSS file for the HomePage component
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import ProductCard from './ProductCard';
+import './HomePage.css';
 
-
-// note: navbar is automatically displayed on top of it because it is in the app.js file top. In app, we direct to this file.
 const HomePage = () => {
-  // Sample data for popular items
-  const popularItems = [
-    { id: 1, name: 'Dog Food', price: '$25', imageUrl: 'path/to/image1.jpg' },
-    { id: 2, name: 'Cat Litter', price: '$15', imageUrl: 'path/to/image2.jpg' },
-    { id: 3, name: 'Dog Toy', price: '$10', imageUrl: 'path/to/image3.jpg' },
-    { id: 4, name: 'Cat Bed', price: '$40', imageUrl: 'path/to/image4.jpg' },
-  ];
+  // products state is used to store the list of products fetched from the backend
+  const [products, setProducts] = useState([]);
+  const BACKEND_URL = 'http://127.0.0.1:8000';
 
+  // useEffect hook is used to fetch products from the backend when the component is mounted to the DOM (whenever, homepage is visited)
+  useEffect(() => {
+    // Fetch products from the backend by sending get request to /products endpoint which returns a list of products as json format
+    const fetchProducts = async () => {
+      try {
+        // response is whole respıonse object, response.data is the data we get from the response which is a list of products each as dictionary object
+        const response = await axios.get(`${BACKEND_URL}/products`);
+        setProducts(response.data);
+      } 
+      catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
 
-  /* 
-  Popular items data will be displayed in the HomePage component. But we need to have PopularItem component to display each item.
-
-  <div className="row">
-        {
-        popularItems.map(item => (
-          <div className="col-md-3 mb-4" key={item.id}>
-            <PopularItem item={item} />
-          </div>
-        ))
-        }
-  </div>
-  */
+    fetchProducts();
+  }, []);
+  
+  const getImageUrl = (imageUrl) => {    
+      // imageUrl is the URL of the product image on the database which is something like : "images/dog-food.jpg"
+      return `${BACKEND_URL}/static/${imageUrl}`;
+    };
 
   return (
-    <div className="container text-center">
+    <div className="homepage-container">
       <h1>Welcome to MyVet!</h1>
       <p>Your one-stop shop for all your pet needs.</p>
-      
-      <h2>Popular Items</h2>
-
-      
-
+      {/* For each of the dictionary in the response.data which is each product, is displayed using ProductCards */}
+      <div className="products-grid">
+        {products.map(product => (
+          <ProductCard 
+            key={product.product_id}
+            name={product.name}
+            model={product.model}
+            description={product.description}
+            quantity={product.quantity}
+            distributor={product.distributor}
+            imageUrl={getImageUrl(product.image_url)}
+          />
+        ))}
+      </div>
     </div>
   );
 };
